f36ebf28ac7bb3deb6e6b934f87ce7e4ed0c8c46
bah-ios <bahran.solomon@astu.edu.et>
2026-09-01 04:17:53 -0400

    scanning range of scans

--- port_scanner.py
+++ port_scanner.py
@@ -1,12 +1,15 @@
 import socket
 
 def scan():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ### AF_INET  means use IPV4 and SOCK_STREAM  means use TCP 
-    port = int(input("Enter the port you want to scan: "))
-    try:
-        s.connect(("localhost", port))
-        print("open")
-    except ConnectionRefusedError:
-        print("closed")
-for i in range(5):
-    scan()
+    starting_port = int(input("Enter the port you want to scan or the staring range "))
+    dest_port = int(input("Enter Destination port"))
+
+    for port in range(starting_port,dest_port+1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ### AF_INET  means use IPV4 and SOCK_STREAM  means use TCP 
+        try:
+            s.connect(("localhost", port))
+            print("port {} is Open".format(port))
+        except ConnectionRefusedError:
+            print("Port {} is closed".format(port))
+            
+scan()
