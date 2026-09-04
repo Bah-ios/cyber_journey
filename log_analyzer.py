d7cfd4c0915e4473175bd920dd0bec0df2beba92
bah-ios <bachadebele@gmail.com>
2026-09-04 17:41:46 -0400

    brute_force detector

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,4 +1,10 @@
 with open("sample.log", 'r') as file:
+    count = 0
     for line in file:
         if "Failed password" in line:
-            print(line)
+            count +=1
+    print("Failed login attempts: {}".format(count))
+    if count >= 3:
+        print("ALERT: possible brute-force attempt detected")
+    else:
+        print("Activity within normal range")
